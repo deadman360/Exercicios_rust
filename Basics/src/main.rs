d2a654ee35d85b47ecca5a03016028b8489c969a
deadman360 <vmartinho.kfg@gmail.com>
2024-11-05 19:42:09 +0000

    Basics Fechado

--- Basics/src/main.rs
+++ Basics/src/main.rs
@@ -1,10 +1,25 @@
 use std::io;
 fn main() {
     saudacoes();
-    calculadora();
 }
 fn saudacoes(){
-    println!("Hello, World");
+    let mut opcao: String =  String::new();
+    println!("Olá, qual código voce gostaria de executar ?
+    \n1-Calculadora
+    \n2-Conversor de Temperatura
+    \n3-Par ou Impar ?
+    \n4- Gerar Tabuada");
+    io::stdin()
+        .read_line(&mut opcao)
+        .expect("Deu ruim");
+    
+    match opcao.trim(){
+        "1" => calculadora(),
+        "2" => conversor_temperatura(),
+        "3" => par_impar(),
+        "4" => tabuada(),
+        _ => println!("Todo")
+    }
 }
 fn calculadora(){
     let mut input: String = String::new();
@@ -25,13 +40,62 @@ fn calculadora(){
             .read_line(&mut operacao)
             .expect("Falaha ao ler sua entrada");
         
-    let n1: i32 = input.trim().parse().expect("Falha interna");
-    let n2: i32 = input2.trim().parse().expect("Falha interna");
+    let n1: f64 = input.trim().parse().expect("Falha interna");
+    let n2: f64 = input2.trim().parse().expect("Falha interna");
     match operacao.trim() {
-        "1" => println!("{}",n1 + n2,),
-        "2" => println!("{}",n1 - n2),
-        "3" => println!("{}",n1 / n2),
-        "4" => println!("{}",n1 * n2),
+        "1" => println!("{:.2}",n1 + n2,),
+        "2" => println!("{:.2}",n1 - n2),
+        "3" if n2 !=0f64 => println!("{:.2}",n1 / n2),
+        "4" => println!("{:.2}",n1 * n2),
         _ => println!("Operação invalida")
+         
+    }
+}
+fn conversor_temperatura(){
+    let mut opt: String = String::new();
+    let mut tmp: String = String::new();
+
+    println!("Olá, qual operecação deseja realizar
+    \n 1- Celsius -> Farenheit
+    \n 2- Farenheit -> Celsius");
+    io::stdin()
+        .read_line(&mut opt)
+        .expect("Erro");
+    
+    println!("Quanto é a temperatura?");
+    io::stdin()
+        .read_line(&mut tmp)
+        .expect("Erro!");
+    let tmp_n: f64 = tmp.trim().parse().expect("Erro");
+    match opt.trim() {
+        "1" => println!("Temperatura em Farenheit = {:.3}º", (tmp_n*9.0/5.0)+32f64),
+        "2" => println!("Temperatura em Celsius = {:.3}º",(tmp_n - 32f64) * 5.0/9.0),
+        _ => println!("Operação inválida")
+
+    }
+}
+fn par_impar(){
+    println!("Quer número gostaria de verificar ?");
+    let mut opt: String = String::new();
+    io::stdin()
+        .read_line(&mut opt)
+        .expect("Erro!");
+    let num: i32 = opt.trim().parse().expect("Numero Invalido!!");
+    match num%2 {
+        1 => println!("Impar"),
+        0 => println!("Par"),
+        _ => println!("Unknown")
+    } 
+}
+fn tabuada(){
+    let mut opt: String = String::new();
+    println!("De qual numero gostaria de saber a tabuada ?");
+    io::stdin()
+        .read_line(&mut opt)
+        .expect("Erro!");
+
+    let num: i32 = opt.trim().parse().expect("Numero Invalido!!!!");
+    for i in 0..=10{
+        println!("{} x {} = {}", num, i, num * i);
     }
 }
